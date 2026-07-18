6314aca6d24c3af5f4b246f5ead22869bf574587
nasirhussainm7799-afk <nasirhussainm7799@gmail.com>
2026-07-18 13:24:59 +0500

    Connected frontend with backend

--- frontend/frontend/script.js
+++ frontend/frontend/script.js
@@ -1207,6 +1207,40 @@ function animateAI(){
 
 
 }
+// NH AI Backend Connection
+
+async function sendToBackend(message){
+
+    try{
+
+        const response = await fetch(
+            "http://localhost:5000/chat",
+            {
+                method:"POST",
+
+                headers:{
+                    "Content-Type":"application/json"
+                },
+
+                body:JSON.stringify({
+                    message: message
+                })
+            }
+        );
+
+
+        const data = await response.json();
+
+        return data.reply;
+
+
+    }catch(error){
+
+        return "NH AI server se connection nahi ho saka.";
+
+    }
+
+}
 
 
 
